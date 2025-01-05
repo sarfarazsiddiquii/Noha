22255616a719b6ea70881c49bea584f3d826054e
sarfaraz siddiqui <sarfarazsiddiqui199@gmail.com>
2025-01-05 18:04:49 +0530

    folders

--- Backend/config.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-class Config:
-    SECRET_KEY = "your_secret_key"
-    MONGO_URI = "mongodb+srv://sarfaraz:gVEJAWpJYJceV60Q@cluster0.eacpy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    client = MongoClient(MONGO_URI)
-    db = client['user_auth']
-    users_collection = db['users']
-    meetings_collection = db['meetings']
